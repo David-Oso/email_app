8367fc02370e5122f847d2c38b9e0aac01af3594
David <osodavid001@gmail.com>
2023-06-27 16:16:51 +0100

    commit

--- mini_mailing_app-spring-boot/src/main/java/com/mail/mini_mailing_app/spring/boot/config/security/JwtService.java
+++ mini_mailing_app-spring-boot/src/main/java/com/mail/mini_mailing_app/spring/boot/config/security/JwtService.java
@@ -1,81 +1,81 @@
-package com.mail.mini_mailing_app.spring.boot.config.security;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-
-import java.security.Key;
-import java.time.Instant;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-
-@Service
-public class JwtService {
-    @Value("${jwtSecret}")
-    private String secretKey;
-    @Value("${accessTokenExpiration}")
-    private long accessTokenExpiration;
-    @Value("${refreshTokenExpiration}")
-    private long refreshTokenExpiration;
-
-    public String extractUsername(String token){
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-
-    public String generateAccessToken(UserDetails userDetails){
-        return generateToken(new HashMap<>(), userDetails);
-    }
-    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return buildToken(extraClaims, userDetails, accessTokenExpiration);
-    }
-    public String generateRefreshToken(UserDetails userDetails){
-        return buildToken(new HashMap<>(), userDetails, refreshTokenExpiration);
-    }
-
-    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
-        return Jwts.builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(Date.from(Instant.now().plusSeconds(expiration)))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS512)
-                .compact();
-    }
-
-    public boolean isTokenValid(String token, UserDetails userDetails){
-        final String userName = extractUsername(token);
-        return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
-    }
-
-    private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-    private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-}
+//package com.mail.mini_mailing_app.spring.boot.config.security;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.io.Decoders;
+//import io.jsonwebtoken.security.Keys;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.stereotype.Service;
+//
+//import java.security.Key;
+//import java.time.Instant;
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.function.Function;
+//
+//@Service
+//public class JwtService {
+//    @Value("${jwtSecret}")
+//    private String secretKey;
+//    @Value("${accessTokenExpiration}")
+//    private long accessTokenExpiration;
+//    @Value("${refreshTokenExpiration}")
+//    private long refreshTokenExpiration;
+//
+//    public String extractUsername(String token){
+//        return extractClaim(token, Claims::getSubject);
+//    }
+//
+//    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+//        final Claims claims = extractAllClaims(token);
+//        return claimsResolver.apply(claims);
+//    }
+//
+//    public String generateAccessToken(UserDetails userDetails){
+//        return generateToken(new HashMap<>(), userDetails);
+//    }
+//    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+//        return buildToken(extraClaims, userDetails, accessTokenExpiration);
+//    }
+//    public String generateRefreshToken(UserDetails userDetails){
+//        return buildToken(new HashMap<>(), userDetails, refreshTokenExpiration);
+//    }
+//
+//    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
+//        return Jwts.builder()
+//                .setClaims(extraClaims)
+//                .setSubject(userDetails.getUsername())
+//                .setIssuedAt(new Date())
+//                .setExpiration(Date.from(Instant.now().plusSeconds(expiration)))
+//                .signWith(getSignInKey(), SignatureAlgorithm.HS512)
+//                .compact();
+//    }
+//
+//    public boolean isTokenValid(String token, UserDetails userDetails){
+//        final String userName = extractUsername(token);
+//        return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
+//    }
+//
+//    private boolean isTokenExpired(String token) {
+//        return extractExpiration(token).before(new Date());
+//    }
+//
+//    private Date extractExpiration(String token) {
+//        return extractClaim(token, Claims::getExpiration);
+//    }
+//
+//    private Claims extractAllClaims(String token) {
+//        return Jwts.parserBuilder()
+//                .setSigningKey(getSignInKey())
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody();
+//    }
+//    private Key getSignInKey() {
+//        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+//        return Keys.hmacShaKeyFor(keyBytes);
+//    }
+//}
--- mini_mailing_app-spring-boot/src/main/java/com/mail/mini_mailing_app/spring/boot/config/security/LogoutService.java
+++ mini_mailing_app-spring-boot/src/main/java/com/mail/mini_mailing_app/spring/boot/config/security/LogoutService.java
@@ -1,33 +1,33 @@
-package com.mail.mini_mailing_app.spring.boot.config.security;
-
-import com.mail.mini_mailing_app.spring.boot.config.security.jwtToken.JwtTokenRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class LogoutService implements LogoutHandler {
-    private final JwtTokenRepository jwtTokenRepository;
-
-    @Override
-    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        if(authentication == null || !authHeader.startsWith("Bearer "))return;
-        jwt = authHeader.substring(7);
-        var storedToken = jwtTokenRepository.findByJwtToken(jwt)
-                .orElse(null);
-        if(storedToken != null){
-//            storedToken.setExpired(true);
-//            storedToken.setRevoked(true);
-//            jwtTokenRepository.save(storedToken);
-            jwtTokenRepository.deleteAll();
-            SecurityContextHolder.clearContext();
-        }
-    }
-}
+//package com.mail.mini_mailing_app.spring.boot.config.security;
+//
+//import com.mail.mini_mailing_app.spring.boot.config.security.jwtToken.JwtTokenRepository;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.web.authentication.logout.LogoutHandler;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class LogoutService implements LogoutHandler {
+//    private final JwtTokenRepository jwtTokenRepository;
+//
+//    @Override
+//    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+//        final String authHeader = request.getHeader("Authorization");
+//        final String jwt;
+//        if(authentication == null || !authHeader.startsWith("Bearer "))return;
+//        jwt = authHeader.substring(7);
+//        var storedToken = jwtTokenRepository.findByJwtToken(jwt)
+//                .orElse(null);
+//        if(storedToken != null){
+////            storedToken.setExpired(true);
+////            storedToken.setRevoked(true);
+////            jwtTokenRepository.save(storedToken);
+//            jwtTokenRepository.deleteAll();
+//            SecurityContextHolder.clearContext();
+//        }
+//    }
+//}
